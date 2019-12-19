229f9f1c56a98a47a85cdcee1fcbb5de1b6a9db6
aapetsi <apetsiampiah@gmail.com>
2019-12-19 20:34:15 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+const app = require('./server')
+
+const port = process.env.PORT
+app.listen(port, () => console.log(`Server is running on port ${port}`))
---
+++ tests/server.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest')
+const app = require('../server')
+
+describe('server file', () => {
+  test('should work', () => {
+    expect(1).toBe(1)
+  })
+})
